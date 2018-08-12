08e9fc9d4c38ab745e889249d6a5bc1a823177dd
Michael Roeper <michael.roeper@t-online.de>
2018-08-12 21:16:35 +0200

    Initial commit

---
+++ routes/auth-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+//auth login
+
+router.get('/login', (req,res) => {
+    res.render('login');
+});
+
+//auth logout
+router.get('/logout', (req,res) =>{
+    //handle with passport
+    res.send('lggong out');
+});
+
+
+router.get('/google', (req,res) => {
+    // handle with passport
+    res.send('logging in with google');
+});
+
+module.exports = router;
